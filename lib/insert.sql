484107bf606c8cada031eb0df7fe8601643f3ac7
isabella g henriques <isabellagh88@gmail.com>
2020-07-03 03:07:10 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 6, 'M', 'black', 'calm', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 18, 'M', 'dark brown', 'grumpy', 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 2, 'F', 'brown', 'calm', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 25, 'M', 'brown', 'calm', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 20, 'F', 'brown', 'calm', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 33, 'M', 'black', 'grumpy', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 40, 'F', 'brown', 'calm', 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 50, 'F', 'brown', 'deadly', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 6, 'M', 'black', 'calm', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Rowdy', 18, 'M', 'dark brown', 'grumpy', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Tabitha', 2, 'F', 'brown', 'calm', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 25, 'M', 'brown', 'calm', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Melissa', 20, 'F', 'brown', 'calm', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Grinch', 33, 'M', 'black', 'grumpy', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('Wendy', 40, 'F', 'brown', 'calm', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, 50, 'F', 'brown', 'deadly', 1);
