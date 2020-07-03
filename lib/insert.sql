2c3d6b261d64ca09410805b5f2c6fa5ce6e3f797
isabella g henriques <isabellagh88@gmail.com>
2020-07-03 03:09:18 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,4 +6,4 @@ INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4,"
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5,"Melissa", 13, "F", "dark brown", "goofy", 1);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7,"Wendy", 6, "F", "Blue", "naive", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, 50, 'F', 'brown', 'deadly', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8,null, 20, "M", "black", "aggressive", 0);
